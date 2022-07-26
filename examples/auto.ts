0feda3f13019fc68802d4544ad29f472a8ba5405
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-26 14:58:41 +0200

    docs: Improved examples

--- examples/auto.ts
+++ examples/auto.ts
@@ -2,11 +2,13 @@
  * Sample usage of `auto`
  *
  * Use `deno run --allow-env --allow-read examples/auto.ts` from the root
- * directory to run this example
+ * directory to run this example. Try using command line arguments to change the
+ * settings!
  */
 
-import { auto } from "../mod.ts";
+import { auto } from "/mod.ts";
 
+// The Config interfaces defines the expected configuration object
 interface Config {
   hostname: string;
   authentication: {
@@ -15,5 +17,7 @@ interface Config {
   };
 }
 
+// Calling `auto` extracts the configuration object from Yaml and json files,
+// environment variables and command line arguments
 const config: Config = await auto({ name: "config", configDir: "examples/" });
 console.log(config);
