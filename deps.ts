a3aa5b05d692c7019bc91ea905f96040a097d735
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-26 20:23:47 +0200

    chores: Replaced rawgithubusercontent with denopkg

--- deps.ts
+++ deps.ts
@@ -5,4 +5,4 @@ export * as path from "https://deno.land/std@0.149.0/path/mod.ts";
 export * as flags from "https://deno.land/std@0.149.0/flags/mod.ts";
 
 // Third party modules
-export * as lodash from "https://raw.githubusercontent.com/lodash/lodash/4.17.21-es/lodash.js";
+export * as lodash from "https://denopkg.com/lodash/lodash@4.17.21-es/lodash.js";
