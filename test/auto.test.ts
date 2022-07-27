2156f0df16ddbdf38bb5722e434a5604bf52720d
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-27 15:58:30 +0200

    tests: created tests for `auto`

---
+++ test/auto.test.ts
@@ -0,0 +1,21 @@
+import { asserts } from "../deps.ts";
+import { auto } from "../mod.ts";
+
+const RESOURCES_FOLDER = "test/resources";
+
+Deno.test("`auto` expected behaviour", async () => {
+  const expected = {
+    "env": "loaded",
+    "base": "loaded",
+    "config.yaml": "loaded",
+    "config.json": "loaded",
+  };
+  Deno.env.set("config_env", "loaded")
+  const obtained: Partial<typeof expected> = await auto({
+    name: "config",
+    configDir: `${RESOURCES_FOLDER}/auto`,
+    baseConfig: {"base": "loaded"}
+  });
+
+  asserts.assertEquals(obtained, expected)
+});
