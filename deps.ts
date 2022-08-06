fb9abe7ef60ba7fad48d3281be9bcc9025722c70
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-08-06 14:46:12 +0200

    feat: Using `std/collections/deep_merge` instead of lodash deep_merge

--- deps.ts
+++ deps.ts
@@ -3,6 +3,6 @@ export * as asserts from "https://deno.land/std@0.151.0/testing/asserts.ts#^";
 export * as yaml from "https://deno.land/std@0.151.0/encoding/yaml.ts#^";
 export * as path from "https://deno.land/std@0.151.0/path/mod.ts#^";
 export * as flags from "https://deno.land/std@0.151.0/flags/mod.ts#^";
+import { deepMerge } from "https://deno.land/std@0.151.0/collections/deep_merge.ts#^";
 
-// Third party modules
-export * as lodash from "https://denopkg.com/lodash/lodash@4.17.21-es/lodash.js#=";
+export const collections = { deepMerge }
