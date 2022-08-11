77c1c07950d714a23b675ca5cbc9f6ecabfa7b43
GitHub <noreply@github.com>
2022-08-11 19:44:43 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 // Standard library
-export * as asserts from "https://deno.land/std@0.151.0/testing/asserts.ts#^";
-export * as yaml from "https://deno.land/std@0.151.0/encoding/yaml.ts#^";
-export * as path from "https://deno.land/std@0.151.0/path/mod.ts#^";
-export * as flags from "https://deno.land/std@0.151.0/flags/mod.ts#^";
-import { deepMerge } from "https://deno.land/std@0.151.0/collections/deep_merge.ts#^";
+export * as asserts from "https://deno.land/std@0.152.0/testing/asserts.ts#^";
+export * as yaml from "https://deno.land/std@0.152.0/encoding/yaml.ts#^";
+export * as path from "https://deno.land/std@0.152.0/path/mod.ts#^";
+export * as flags from "https://deno.land/std@0.152.0/flags/mod.ts#^";
+import { deepMerge } from "https://deno.land/std@0.152.0/collections/deep_merge.ts#^";
 
 export const collections = { deepMerge };
