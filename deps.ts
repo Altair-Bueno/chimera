2e4c6ed9a016ebfdfcd540e20e37572589adcb39
GitHub <noreply@github.com>
2022-11-13 19:43:52 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * as yaml from "https://deno.land/std@0.163.0/encoding/yaml.ts#^";
-export * as path from "https://deno.land/std@0.163.0/path/mod.ts#^";
-export * as flags from "https://deno.land/std@0.163.0/flags/mod.ts#^";
-import { deepMerge } from "https://deno.land/std@0.163.0/collections/deep_merge.ts#^";
+export * as yaml from "https://deno.land/std@0.164.0/encoding/yaml.ts#^";
+export * as path from "https://deno.land/std@0.164.0/path/mod.ts#^";
+export * as flags from "https://deno.land/std@0.164.0/flags/mod.ts#^";
+import { deepMerge } from "https://deno.land/std@0.164.0/collections/deep_merge.ts#^";
 
 export const collections = { deepMerge };
